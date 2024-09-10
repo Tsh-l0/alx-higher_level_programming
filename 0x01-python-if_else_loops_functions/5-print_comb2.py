de5851f3e069f3754151e8e6ea56eb69637fd9b5
Pule Molefe <kodingwithtsh0l0@gmail.com>
2024-09-10 14:34:36 -0700

    Print double digits with a comma

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for d_digits in range(100):
     if d_digits < 99:
-        print(f"{d_digits:02d}, ", end="") #Print nos with 2 digits + ','
+        print("{:02d}, ".format(d_digits), end="")
     else:
-        print(f"{d_digits:02d}") #Print last number + newline
+        print("{:02d}".format(d_digits))
