ce00d722c11b6317611898170ce63f0a7b1377fb
Pule Molefe <kodingwithtsh0l0@gmail.com>
2024-09-11 04:43:12 -0700

    Print ASCII values in reverse order

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for alpha in range(122, 96, -1):
-    print("{}".format(chr(alpha) if alpha % 2 == 0 else chr(alpha - 32)), end ="")
+for alpha in range(ord('z'), ord('a') - 1, -1):
+    if (alpha % 2 != 0):
+        print("{}".format(chr(alpha)), end="")
+    else:
+        print("{}".format(chr(alpha - 32)), end="")
