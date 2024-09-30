41c798055d26694c55fa7283841741f17ad997d3
Pule Molefe <kodingwithtsh0l0@gmail.com>
2024-09-30 07:46:57 -0700

    prints x elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     total = 0
-    for e in range (x):
+    for e in range(x):
         try:
             print(f"{my_list[e]}", end="")
             total += 1
