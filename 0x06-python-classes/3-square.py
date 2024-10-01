64a359f97c83b13e2f4cca85f0360e96ca63146b
Pule Molefe <kodingwithtsh0l0@gmail.com>
2024-10-01 14:01:16 -0700

    Define a class with a private instance attribute, raise errors and define a public instance method

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,26 +3,29 @@
 
 
 class Square:
+    """
+    A class used to represent a Square
 
+    __size : int
+        the size of the square's side
+    """
     def __init__(self, size=0):
-
-        """Initializes the class, Square.
-        Args:
-            size: Represents the size of the square
+        """
+        size: int
+            The size of the square's side
         Raises:
-            TypeError: if size is not int
-            ValueError: if size < 0
+            TypeError: If size is not an int
+            ValueError: If size < 0
         """
         if not isinstance(size, int):
-            raise TypeError('size must be an integer')
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
         self.__size = size
 
-    def area(self):
-        """
-        Calculates the area of the classm Square
-        Returns: The size of the class, Square
-        """
-
-        return self.__size ** 2
+        def area(self):
+            """
+            Returns:
+                int : The area of the square
+            """
+            return self.__size ** 2
