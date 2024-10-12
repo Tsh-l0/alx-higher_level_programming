c2d995006c7eccaf7f2ef313671d600402239864
Pule Molefe <kodingwithtsh0l0@gmail.com>
2024-10-11 19:29:03 -0700

    Run unittests for the function def max_integer(list=[])

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,11 +12,11 @@ class TestMaxInteger(unittest.TestCase):
     Define unittest
     """
 
-    def max_at_end(self):
-        self.assertEqual(max_integer([2, 4, 6, 8]), 4)
+    def test_max_at_end(self):
+        self.assertEqual(max_integer([2, 4, 6, 8]), 8)
 
-    def max_at_middle(self):
-         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+    def test_max_at_middle(self):
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
 
     def test_empty_str(self):
         """Test an empty str"""
@@ -25,14 +25,18 @@ class TestMaxInteger(unittest.TestCase):
     def test_list_str(self):
         """Tests a list of str"""
         sentence = ["python", "is", "my", "favourite"]
-        self.assertEqual(max_integer(sentence), "favourite")
+        self.assertEqual(max_integer(sentence), "python")
 
     def test_str(self):
         """Tests for a str"""
         word = "python"
-        self.assertEqual(max_integer(word), "p")
+        self.assertEqual(max_integer(word), "y")
 
     def test_float(self):
         """Tests for floats"""
-        float = [1.12, 2.24, 3.14, 4.45. 5.56]
-        self.assertEqual(max_intetger(float), 3.14)
+        float = [1.12, 2.24, 3.14, 4.45, 5.56]
+        self.assertEqual(max_integer(float), 5.56)
+
+
+if __name__ == '__main__':
+    unittest.main()
