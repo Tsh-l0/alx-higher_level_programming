293720d3a2a92f644d234ca2aee278ffb6feb5e1
Pule Molefe <kodingwithtsh0l0@gmail.com>
2024-10-14 00:08:06 -0700

    Write a class with a Public instance method that raises an exception with a message

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Writes a class, BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Class, BaseGeometry
+    """
+    def area(self):
+        """
+        Raises an exception with a message when called
+        """
+        raise Exception("area() is not implemented")
