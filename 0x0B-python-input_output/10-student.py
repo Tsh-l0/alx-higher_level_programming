d761c02516ffb93a96cee8f076c6ddbcfe396eee
Pule Molefe <kodingwithtsh0l0@gmail.com>
2024-10-15 07:55:59 -0700

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A class that defines a student using attributes
+"""
+
+
+class Student:
+    """
+    Class, Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the class student using the attributes in the args
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns the dictionary representation of the class, Student
+        """
+        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
