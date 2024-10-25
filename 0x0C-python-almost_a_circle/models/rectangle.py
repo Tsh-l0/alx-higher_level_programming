106f2d2f261ccf3418eea9daccbdfd2533839f54
Pule T Molefe <pmolefe25@gmail.com>
2024-10-25 15:48:09 -0700

    Doc string adding

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,6 +8,9 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """
+    Represents the class, Rectangle
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
