dd0fba3ded3d2d52386fc080e33507a2502a479d
Pule Molefe <kodingwithtsh0l0@gmail.com>
2024-11-19 20:31:41 -0800

    List all the privileges of MySQL users on localhost

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,8 @@
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on
+-- the local server
+
+-- List the privileges of user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List the privileges of user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
