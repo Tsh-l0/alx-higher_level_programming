9ad559876b56eca58ad7b3f52282d58e3e39144b
Pule Molefe <kodingwithtsh0l0@gmail.com>
2024-12-20 15:17:23 -0800

    Prints 3 lines using an array and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Prints 3 lines but by using an array of string and a loop
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let ab = 0; ab < languages.length; ab++) {
+  console.log(languages[ab]);
+}
