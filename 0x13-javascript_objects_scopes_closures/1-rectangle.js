cd394adf61c2bd50e85e4791f01f0a634d3094e4
Pule Molefe <kodingwithtsh0l0@gmail.com>
2024-12-21 11:34:16 -0800

    Writes a class Rectangle that defines a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Writes a class Rectangle that defines a rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.width = h;
+  }
+}
+module.exports = Rectangle;
