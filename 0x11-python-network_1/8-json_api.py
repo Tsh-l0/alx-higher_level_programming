86c960e728ed3aa3ffc01c4e66211c015bf75101
Pule Molefe <pmolefe25@gmail.com.com>
2025-01-17 19:26:39 -0800

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    r = sys.argv[1] if len(sys.argv) > 1 else ""
+    data = {'r': r}
+    response = requests.post("http://0.0.0.0:5000/search_user", data=data)
+
+    try:
+        json_response = response.json()
+        if json_response:
+            print(f"[{json_response.get('id')}] {json_response.get('name')}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
